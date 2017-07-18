b4109d70d2bd22ab41016d938dd48034d5107852
izufic <ivanzufic@gmail.com>
2017-07-18 15:05:28 +0200

    initial commit

--- src/main/java/rproject/Player.java
+++ src/main/java/rproject/Player.java
@@ -93,14 +93,14 @@ public class Player {
 		return cntAttUnits > defTerritory.getUnits();
 	}
 
-	private boolean attackPhase() {
+	public boolean attackPhase() {
 		boolean getBonus = false;
-		System.out.print("attack? y/n");
+		System.out.println("attack? y/n");
 		char response = sc.next().charAt(0);
 		while(response == 'y'){
 			Board board = BoardProvider.getBoard();
 			board.getMatrix().drawMatrixCUI();
-			System.out.print("attack from a to b using n units, write using format: a b n\n");
+			System.out.println("attack from a to b using n units, write using format: a b n");
 			String attTerritoryName = sc.next();
 			Territory attTerritory = board.getTerritory(attTerritoryName);
 			String defTerritoryName = sc.next();
@@ -111,7 +111,7 @@ public class Player {
 					getBonus = true;
 					defTerritory.changeOwner(this);
 				}
-			System.out.print("attack again? y/n");
+			System.out.println("attack again? y/n");
 			response = sc.next().charAt(0);
 		}
 		return getBonus;
@@ -141,22 +141,22 @@ public class Player {
 		return true;
 	}
 
-	private void movePhase(){
-		System.out.print("move units? y/n");
+	public void movePhase(){
+		System.out.println("move units? y/n");
 		char response = sc.next().charAt(0);
 		while(response == 'y'){
 			Board board = BoardProvider.getBoard();
 			board.getMatrix().drawMatrixCUI();
-			System.out.print("move n units from a to b, write using format: a b n\n");
+			System.out.println("move n units from a to b, write using format: a b n");
 			String startingTerritoryName = sc.next();
-			Territory startingTerritory = board.getTerritory(startingTerritoryName);
+			Territory starting = board.getTerritory(startingTerritoryName);
 			String endingTerritoryName = sc.next();
-			Territory endingTerritory = board.getTerritory(endingTerritoryName);
+			Territory ending = board.getTerritory(endingTerritoryName);
 			int cntMovingUnits = sc.nextInt();
-			if (checkValidMoving(startingTerritory, endingTerritory, cntMovingUnits)){
-				startingTerritory.moveUnits(endingTerritory, cntMovingUnits);
+			if (checkValidMoving(starting, ending, cntMovingUnits)){
+				starting.moveUnits(ending, cntMovingUnits);
 			}
-			System.out.print("move again? y/n");
+			System.out.println("move again? y/n");
 			response = sc.next().charAt(0);
 		}
 	}
@@ -165,12 +165,12 @@ public class Player {
 		return 3; // todo
 	}
 
-	private boolean checkValidSpawning(Territory spawnTerritory, int cntSpawnUnits){
-		if (spawnTerritory == null){
+	private boolean checkValidSpawning(Territory spawn, int cntSpawnUnits){
+		if (spawn == null){
 			System.out.println("invalid name of the territory");
 			return false;
 		}
-		if (!spawnTerritory.getOwner().getName().equals(this.getName())){
+		if (!spawn.getOwner().getName().equals(this.getName())){
 			System.out.println("territory doesn't belong to you");
 			return false;
 		}
@@ -181,27 +181,27 @@ public class Player {
 		return true;
 	}
 
-	private void spawnPhase(){
+	public void spawnPhase(){
 		cntExtraUnits += getSpawnCount();
-		System.out.print("spawn units? y/n");
+		System.out.println("spawn units? y/n");
 		char response = sc.next().charAt(0);
 		while(response == 'y'){
 			Board board = BoardProvider.getBoard();
 			board.getMatrix().drawMatrixCUI();
-			System.out.printf("number of available units: %d\n",cntExtraUnits);
-			System.out.print("spawn n units on territory a, write using format: a n\n");
+			System.out.println("number of available units: " + cntExtraUnits);
+			System.out.println("spawn n units on territory a, write using format: a n");
 			String spawnTerritoryName = sc.next();
 			Territory spawnTerritory = board.getTerritory(spawnTerritoryName);
 			int cntSpawningUnits = sc.nextInt();
 			if (checkValidSpawning(spawnTerritory, cntSpawningUnits)){
 				spawnTerritory.addUnits(cntSpawningUnits);
 			}
-			System.out.print("spawn again? y/n");
+			System.out.println("spawn again? y/n");
 			response = sc.next().charAt(0);
 		}
 	}
 
-	private void bonusPhase(){
+	public void bonusPhase(){
 		cntBonus++;
 	}
 }

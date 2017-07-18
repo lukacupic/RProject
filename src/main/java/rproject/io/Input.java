92791ef0aaa7c18a3199abcd1babec9f013b6ee5
Luka <lukacupic42@gmail.com>
2017-07-18 20:26:03 +0200

    Initial commit.

--- src/main/java/rproject/io/Input.java
+++ src/main/java/rproject/io/Input.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 
 public class Input {
 	static Scanner sc = new Scanner(System.in);
+
 	public static String readString() {
 		String input = sc.next();
 		return input;
 	}
+
 	public static int readInt() {
 		int input = sc.nextInt();
 		return input;
+	}
+
+	/**
+	 * Returns a character token read from the user input. Each
+	 * read character is returned in the lower-case format thus
+	 * making user input case insensitive for easier processing.
+	 *
+	 * @return the next character from the user input
+	 */
+	public static char readChar() {
+		return readString().charAt(0);
 	}
 }
--- src/main/java/rproject/io/Output.java
+++ src/main/java/rproject/io/Output.java
@@ -1,8 +1,21 @@
 package rproject.io;
 
+import java.util.Arrays;
+
 public class Output {
+
+	public static char PROMPT_SYMBOL = '>';
+
 	public static void write(String str) { System.out.print(str); }
 	public static void writeln(String str) { System.out.println(str); }
 	public static void write(int num) { System.out.print(num); }
 	public static void writeln(int num) { System.out.println(num); }
+
+	/**
+	 * Writes a prompt symbol onto the output, indicating that a user
+	 * interaction input is wanted.
+	 */
+	public static void prompt() {
+		writeln(PROMPT_SYMBOL + " ");
+	}
 }

8d70a29905b73d76c14a056ad8bb69da733c3f9b
Luka <lukacupic42@gmail.com>
2017-07-18 12:15:35 +0200

    Initial commit.

--- src/main/java/rproject/NeighbourhoodMatrix.java
+++ src/main/java/rproject/NeighbourhoodMatrix.java
@@ -9,13 +9,19 @@ public class NeighbourhoodMatrix {
 	private int size;
 	private boolean[][] neighbourhoodMatrix;
 
-	public NeighbourhoodMatrix(String FileName) throws IOException {
-		List<String> Lines = Files.readAllLines(Paths.get("maps/matrix/" + FileName + ".txt"));
-		size = Integer.parseInt(Lines.get(0));
+	public NeighbourhoodMatrix(String FileName) {
+		List<String> lines;
+		try {
+			lines = Files.readAllLines(Paths.get("maps/matrix/" + FileName + ".txt"));
+		} catch (IOException ex) {
+			throw new RuntimeException();
+		}
+
+		size = Integer.parseInt(lines.get(0));
 		neighbourhoodMatrix = new boolean[size][size];
 		for (int i = 0; i < size; ++i)
 			for (int j = 0; j < size; ++j)
-				neighbourhoodMatrix[i][j] = (Lines.get(i + 1).charAt(j) == '1');
+				neighbourhoodMatrix[i][j] = (lines.get(i + 1).charAt(j) == '1');
 	}
 
 	public void drawMatrixCUI() throws Exception {

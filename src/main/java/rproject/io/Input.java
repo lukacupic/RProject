2cce279810b5cc88b08b53b7e64581e14b0138c3
izufic <ivanzufic@gmail.com>
2017-07-18 20:35:18 +0200

    initial commit

--- src/main/java/rproject/io/Input.java
+++ src/main/java/rproject/io/Input.java
@@ -3,16 +3,14 @@ package rproject.io;
 import java.util.Scanner;
 
 public class Input {
-	static Scanner sc = new Scanner(System.in);
+	private static Scanner sc = new Scanner(System.in);
 
 	public static String readString() {
-		String input = sc.next();
-		return input;
+		return sc.next();
 	}
 
 	public static int readInt() {
-		int input = sc.nextInt();
-		return input;
+		return sc.nextInt();
 	}
 
 	/**

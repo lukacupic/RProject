e3ad15ff08396a8fa3c02ba3a8a61fd6e49fa5f3
Luka <lukacupic42@gmail.com>
2017-07-19 13:15:35 +0200

    Initial commit.

--- src/main/java/rproject/util/Util.java
+++ src/main/java/rproject/util/Util.java
@@ -3,5 +3,14 @@ package rproject.util;
 import java.util.Random;
 
 public class Util {
-	public final static Random rand = new Random();
+
+	private final static Random rand = new Random();
+
+	public int getRandInt(int start, int end) {
+		return (Math.abs(rand.nextInt(end - start) + start));
+	}
+
+	public int getRandInt(int end) {
+		return getRandInt(0, end);
+	}
 }

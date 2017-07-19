6500645719d8bf2930a96d03d725d88dcf3afeba
izufic <ivanzufic@gmail.com>
2017-07-19 13:20:31 +0200

    Merge branch 'master' of https://github.com/lukacupic/RProject
    Conflicts:
    	src/main/java/rproject/Game.java

--- src/main/java/rproject/util/Util.java
+++ src/main/java/rproject/util/Util.java
@@ -6,11 +6,11 @@ public class Util {
 
 	private final static Random rand = new Random();
 
-	public int getRandInt(int low, int high) {
+	public static int getRandInt(int low, int high) {
 		return rand.nextInt(high - low) + low;
 	}
 
-	public int getRandInt(int high) {
+	public static int getRandInt(int high) {
 		return getRandInt(0, high);
 	}
 }

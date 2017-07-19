3618d4f08d5610561f65fff8c15e4b259837e0de
Luka <lukacupic42@gmail.com>
2017-07-19 12:57:26 +0200

    Initial commit.

--- src/main/java/rproject/units/Fighter.java
+++ src/main/java/rproject/units/Fighter.java
@@ -4,11 +4,11 @@ public class Fighter extends Unit {
 
 	public Fighter() {
 		hp = 100;
-		damage = 100;
+		damage = 10;
 	}
 
 	@Override
 	public void attack(Unit unit) {
-
+		unit.removeHp(damage);
 	}
 }

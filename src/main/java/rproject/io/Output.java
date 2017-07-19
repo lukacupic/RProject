6838fc8e26a5646095dacb6972271f7614f53c1d
Luka <lukacupic42@gmail.com>
2017-07-19 11:23:52 +0200

    Initial commit.

--- src/main/java/rproject/io/Output.java
+++ src/main/java/rproject/io/Output.java
@@ -1,12 +1,10 @@
 package rproject.io;
 
-import java.util.Arrays;
-
 public class Output {
 
 	public static char PROMPT_SYMBOL = '>';
 
-	public static boolean write(String str) { System.out.print(str); return true; }
+	public static void write(String str) { System.out.print(str); }
 	public static void writeln(String str) { System.out.println(str); }
 	public static void write(int num) { System.out.print(num); }
 	public static void writeln(int num) { System.out.println(num); }

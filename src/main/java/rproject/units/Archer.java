358554047c59463bbc9fb736c491cf45b5a3fd46
izufic <ivanzufic@gmail.com>
2017-07-26 17:08:07 +0200

    add javadoc

--- src/main/java/rproject/units/Archer.java
+++ src/main/java/rproject/units/Archer.java
@@ -2,12 +2,18 @@ package rproject.units;
 
 public class Archer extends Unit {
 
+	/**
+	 * Default stats for every archer
+	 */
  	private static final int DEFAULT_HP = 60;
 	private static final int DEFAULT_DMG = 35;
 	private static final int DEFAULT_PRICE = 2;
 	private static final int DEFAULT_ARMOR = 0;
 	private static final int DEFAULT_TARGETED_CHANCE_COEF = 35;
 
+	/**
+	 * Constructor
+	 */
 	public Archer() {
 		hp = DEFAULT_HP;
 		damage = DEFAULT_DMG;
